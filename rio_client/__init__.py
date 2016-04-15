776b0d0eda2a9da1e79d1e69ef04c384be33b6b4
soasme <soasme@gmail.com>
2016-04-15 15:45:26 +0800

    Bump version: 0.1.1 → 0.1.2

--- rio_client/__init__.py
+++ rio_client/__init__.py
@@ -6,4 +6,4 @@ rio
 
 from .base import Client
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ test_requirements = [
 
 setup(
     name='rio-client',
-    version='0.1.1',
+    version='0.1.2',
     description="Client for Rio.",
     long_description=readme + '\n\n' + history,
     author="Ju Lin",
