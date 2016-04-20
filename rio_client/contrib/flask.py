0bfe4c2e0119c3eeacfd1c51e5f26289aa2a9b0b
soasme <soasme@gmail.com>
2016-04-20 09:34:44 +0800

    add `rio.current` property.

--- rio_client/contrib/flask.py
+++ rio_client/contrib/flask.py
@@ -2,8 +2,15 @@
 
 from __future__ import absolute_import
 
+from collections import namedtuple
+
+from flask import request
+
 from rio_client.base import Client
 
+
+class Current(namedtuple('Current', 'action project uuid')):
+    pass
 
 class Rio(object):
     """Flask extension for rio."""
@@ -29,3 +36,18 @@ class Rio(object):
     def emit(self, action, payload):
         """Emit action."""
         return self.client.emit(action, payload)
+
+    @property
+    def current(self):
+        """A namedtuple contains `uuid`, `project`, `action`.
+
+        Example::
+
+            @app.route('/webhook/broadcast-news')
+            def broadcast_news():
+                if rio.current.action.startswith('news-'):
+                    broadcast(request.get_json())
+        """
+        event = request.headers.get('X-RIO-EVENT')
+        data = dict([elem.split('=') for elem in event.split(',')])
+        return Current(**data)
